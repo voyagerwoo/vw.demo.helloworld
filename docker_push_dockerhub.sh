6ddde617834489593fb9823a8ef9a9c89256c3f8
voyager.woo <voyager.woo@gmail.com>
2018-04-22 13:44:09 +0900

    Add docker login script

--- docker_push_dockerhub.sh
+++ docker_push_dockerhub.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # required docker login
+docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USER} -p ${DOCKER_PASS}
 docker tag vw.demo.helloworld:latest voyagerwoo/vw.demo.helloworld:latest
 docker push voyagerwoo/vw.demo.helloworld:latest
