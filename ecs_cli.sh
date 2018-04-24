a9cbc7d87e0c8470f237f6e27f98b07022a47dad
voyager.woo <voyager.woo@gmail.com>
2018-04-24 12:50:51 +0900

    Update ecs-cli compose script

--- ecs_cli.sh
+++ ecs_cli.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ecs-cli configure --cluster hello-ecs-cli --region ap-northeast-2 \
   --default-launch-type EC2 --config-name hello-ecs-cli
 
@@ -12,14 +14,14 @@ ecs-cli up --keypair voyager.woo \
 
 
 # test compose container up
-ecs-cli compose --file hello-ecs.yml up --create-log-groups --cluster hello-ecs-cli
+ecs-cli compose --file hello-compose.yml up --create-log-groups --cluster hello-ecs-cli
 # test compose scale up
-ecs-cli compose --file hello-ecs.yml scale 2 --cluster hello-ecs-cli
+ecs-cli compose --file hello-compose.yml scale 2 --cluster hello-ecs-cli
 # shutdown compose container
-ecs-cli compose --file hello-ecs.yml down --cluster hello-ecs-cli
+ecs-cli compose --file hello-compose.yml down --cluster hello-ecs-cli
 
 # 서비스 생성
-ecs-cli compose --file hello-ecs.yml service create --cluster hello-ecs-cli \
+ecs-cli compose --file hello-compose.yml service create --cluster hello-ecs-cli \
   --deployment-max-percent 200 \
   --deployment-min-healthy-percent 50 \
   --target-group-arn "arn:aws:elasticloadbalancing:ap-northeast-2:957582603404:targetgroup/hello-ecs-cli-service/13e879e4c11b7f11" \
@@ -29,8 +31,9 @@ ecs-cli compose --file hello-ecs.yml service create --cluster hello-ecs-cli \
   --create-log-groups 
 
 # 서비스 초기화
-ecs-cli compose --file hello-ecs.yml service up --cluster hello-ecs-cli
+ecs-cli compose --file hello-compose.yml service up --cluster hello-ecs-cli
 
-ecs-cli compose --file hello-ecs.yml service scale 2 --cluster hello-ecs-cli 
+ecs-cli compose --file hello-compose.yml service scale 2 --cluster hello-ecs-cli 
 
-ecs-cli compose --file hello-ecs.yml service up --cluster hello-ecs-cli --force-deployment
+ecs-cli compose --file hello-compose.yml --project-name hello-ecs-cli service up \
+  --cluster hello-ecs-cli --force-deployment
